32f18556b9ac82d88aec3404073d89712a2614ac
Yana Flaryanovich <Yana_Flaryanovich@epam.com>
2022-03-01 23:21:42 +0300

    Initial commit

---
+++ Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    [Serializable]
+    public class Department
+    {
+        public string DepartmentName { get; set; }
+
+        [XmlArray("Workers")]
+        [XmlArrayItem("Worker")]
+        [JsonPropertyName("Workers")]
+        public List<Employee> Employees { get; set; }
+    }
+}
---
+++ ISerialization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    public interface ISerialization
+    {
+        void Serialize(Department department, string fileName);
+        Department Deserialize(string fileName);
+    }
+}
