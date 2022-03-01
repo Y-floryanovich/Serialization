2ec91580ba2c7c8bb7dc2a57de4602676e5d3853
Yana Flaryanovich <Yana_Flaryanovich@epam.com>
2022-03-01 23:52:39 +0300

    Task 1

---
+++ JsonSerialization/Json.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Text.Json;
+using Serialization;
+
+namespace JsonSerialization
+{
+    public class Json : ISerialization
+    {
+        public void Serialize(Department department, string fileName)
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            var jsonString = JsonSerializer.Serialize(department, options);
+            File.WriteAllText(fileName, jsonString);
+        }
+
+        public Department Deserialize(string fileName)
+        {
+            var jsonString = File.ReadAllText(fileName);
+            var department = JsonSerializer.Deserialize<Department>(jsonString);
+            return department;
+        }
+    }
+}
