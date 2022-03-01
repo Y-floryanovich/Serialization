cf8826004be1c21c89f915531e2ed24989705216
Yana Flaryanovich <Yana_Flaryanovich@epam.com>
2022-03-02 00:03:04 +0300

    task 2

---
+++ SerializeSimpleClass.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization
+{
+    public class SerializeSimpleClass 
+    {
+        public void Serialize(SimpleClass simpleClass, string fileName)
+        {
+            var formatter = new BinaryFormatter();
+            var stream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, simpleClass);
+            stream.Close();
+        }
+
+        public SimpleClass Deserialize(string fileName)
+        {
+            var formatter = new BinaryFormatter();
+            var stream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+            var simpleClass = (SimpleClass)formatter.Deserialize(stream);
+            stream.Close();
+            return simpleClass;
+        }
+    }
+}
